fc78971a6b12ac3c41884cb7d32eb4e5a9e63146
oginom <oginomaa@gmail.com>
2023-05-02 11:57:23 +0900

    Add features

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,7 +1,47 @@
-import { Layer, Rect, Circle, Ellipse, Line, Stage, Image, Text } from "react-konva";
+/* eslint-disable jsx-a11y/alt-text */
+
+import { Layer, Rect, Circle, Ellipse, Line, Stage, Image, Text, Group } from "react-konva";
 import { Inter } from 'next/font/google'
 import { FC, useCallback, useEffect, useRef, useState } from "react";
 import useImage from 'use-image';
+import { atom, useSetRecoilState, useRecoilValue } from 'recoil';
+
+const SHOW_BOUNDING_BOX = true
+
+const keyboardState = atom({
+  key: 'keyboardState',
+  default: {},
+});
+
+function KeyboardEventHandler() {
+  const setKeyboardState = useSetRecoilState(keyboardState);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      setKeyboardState((prev) => ({
+        ...prev,
+        [event.code]: true,
+      }));
+    };
+
+    const handleKeyUp = (event) => {
+      setKeyboardState((prev) => ({
+        ...prev,
+        [event.code]: false,
+      }));
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
+  return null;
+}
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,26 +58,75 @@ type Player = {
   disp_d: Vec2
   v: Vec2
   jumping: boolean
+  fated: boolean
 }
 
-const PlayerView: FC<Player> = ({ t, pos, disp_d, v, jumping }) => {
+const PLAYER_R = 50
+
+const PlayerView: FC<Player> = (player) => {
   const [image] = useImage('kodai_dodo.png')
-  // eslint-disable-next-line jsx-a11y/alt-text
-  return <Image image={image} x={
-    pos.x + disp_d.x
-  } y={
-    pos.y + disp_d.y
-  } width={150} height={147}/>
+  const [image_fated] = useImage('kodai_dodo_fated.png')
+  const image_now = (player.fated ? image_fated : image)
+  return <Group>
+    <Image image={image_now} x={ player.pos.x + player.disp_d.x - 75 } y={ player.pos.y + player.disp_d.y - 73 } width={150} height={146}/>
+    {/* SHOW_BOUNDING_BOX && <Rect stroke='red' strokeWidth={2} x={player.pos.x - 75} y={player.pos.y - 73} width={150} height={146} /> */}
+    {SHOW_BOUNDING_BOX && <Circle stroke='red' strokeWidth={2} x={player.pos.x} y={player.pos.y} radius={PLAYER_R} />}
+  </Group>
 };
+
+type Charactor = "TIGER" | "VELOCI" | "MICRO"
+
+const imageName = {
+  "TIGER": "animal_fukuroookami_tasmanian_tiger.png",
+  "VELOCI": "Velociraptor.png",
+  "MICRO": "kodai_microraptor.png",
+}
 
 type Enemy = {
   pos: Vec2
+  charactor: Charactor
 }
 
-const EnemyView: FC<Enemy> = ({ pos }) => {
-  const [image] = useImage('animal_fukuroookami_tasmanian_tiger.png')
-  // eslint-disable-next-line jsx-a11y/alt-text
-  return <Image image={image} x={ pos.x } y={ pos.y } width={169} height={139}/>
+const nextEnemy = () => {
+  const enemies: Enemy[] = [
+    {
+      pos: { x: 700, y: 450 + 90 },
+      charactor: "TIGER",
+    },
+    {
+      pos: { x: -100, y: 450 + 70 },
+      charactor: "VELOCI",
+    },
+    {
+      pos: { x: 700, y: 300 },
+      charactor: "MICRO",
+    },
+  ]
+  return enemies[Math.floor(Math.random() * 3)]
+}
+
+const stepEnemy = (enemy: Enemy, dt: number) => {
+  const newEnemy = enemy
+  if (enemy.charactor == "TIGER") {
+    newEnemy.pos.x += dt * (-300)
+  } else if (enemy.charactor == "VELOCI") {
+    newEnemy.pos.x += dt * (300)
+  } else if (enemy.charactor == "MICRO") {
+    newEnemy.pos.x += dt * (-400)
+  }
+  return newEnemy
+}
+
+const ENEMY_R = 50
+
+const EnemyView: FC<Enemy> = (enemy) => {
+  const [image] = useImage(imageName[enemy.charactor])
+  const scaleX = enemy.charactor == "VELOCI" ? -1 : 1
+  return <Group>
+    <Image image={image} x={ enemy.pos.x - 84 * scaleX } y={ enemy.pos.y - 69 } width={168} height={138} scaleX={scaleX} />
+    {/* SHOW_BOUNDING_BOX && <Rect stroke='blue' strokeWidth={2} x={enemy.pos.x - 84} y={enemy.pos.y - 69} width={168} height={138} /> */}
+    {SHOW_BOUNDING_BOX && <Circle stroke='blue' strokeWidth={2} x={enemy.pos.x} y={enemy.pos.y} radius={ENEMY_R} />}
+  </Group>
 };
 
 type Spawner = {
@@ -49,9 +138,17 @@ type Game = {
   spawner: Spawner
   player: Player
   enemies: Enemy[]
+  gameOver: bool
 }
 
-const GameView: FC<Game> = ({ t, player, enemies }) => {
+const RetryButton: FC<any> = (props) => {
+  return <Group>
+    <Rect fill='gray' width={200} height={100} {...props} x={props.x - 100} y={props.y - 50} onClick={props.callback} />
+    <Text text={"RETRY"} {...props} />
+  </Group>
+}
+
+const GameView: FC<Game & {handleRetry: any}> = ({ t, player, enemies, gameOver, handleRetry }) => {
   return <Layer>
       <Rect fill='red' x={100} y={100} width={300} height={200} />
       <Rect fill='red' x={400} y={350} width={50} height={50} />
@@ -65,22 +162,24 @@ const GameView: FC<Game> = ({ t, player, enemies }) => {
       })}
       <PlayerView {...player} />
       <Text x={10} y={10} text={String(Math.floor(t))} />
+      { gameOver && <RetryButton x={300} y={300} callback={handleRetry} /> }
     </Layer>
 }
 
 type GameStage = {
   handleClick: any
+  handleRetry: any
   width: number
   height: number
   game: Game
 }
 
-const GameStageView: FC<GameStage> = ({ handleClick, width, height, game }) => {
+const GameStageView: FC<GameStage> = ({ handleClick, handleRetry, width, height, game }) => {
   return <Stage width={width} height={height} className="flex justify-center" onKeyPress={(e) => console.log(e)} onClick={() => handleClick()}>
     <Layer>
       <Rect stroke='black' strokeWidth={4} x={2} y={2} width={width-4} height={height-4} />
     </Layer>
-    <GameView {...game} />
+    <GameView {...game} handleRetry={handleRetry} />
   </Stage>
 }
 
@@ -100,7 +199,7 @@ const useAnimationFrame = (callback = () => {}) => {
   }, [loop]);
 };
 
-const GND = 550
+const GND = 623
 
 export default function Home() {
 
@@ -111,15 +210,17 @@ export default function Home() {
     spawner: { prevT: 0 },
     player: {
       t: 0,
-      pos: {x: 200, y: GND},
+      pos: {x: 275, y: GND},
       disp_d: {x: 0, y: 0},
       v: {x: 0, y: 0},
       jumping: true,
+      fated: false,
     },
-    enemies: [{
-      pos: {x: 400, y: 450 + 24},
-    }],
+    enemies: [],
+    gameOver: false,
   })
+
+  const currentKeyboardState = useRecoilValue(keyboardState);
 
   useAnimationFrame(() => {
     const dt = 1 / 60.0;
@@ -131,36 +232,43 @@ export default function Home() {
 
     const newPlayer = player
     newPlayer.t += dt
-    if (newPlayer.jumping) {
-      newPlayer.pos.y = player.pos.y + player.v.y * dt
-      if (newPlayer.pos.y + 100 < GND) {
-        newPlayer.v.y += 2000 * dt
+
+    if (!newPlayer.fated) {
+
+      if (newPlayer.jumping) {
+        newPlayer.pos.y = player.pos.y + player.v.y * dt
+        if (newPlayer.pos.y + 100 < GND) {
+          newPlayer.v.y += 1500 * dt
+        } else {
+          newPlayer.jumping = false;
+          newPlayer.pos.y = GND - 100
+          newPlayer.v.y = 0
+        }
+
+        newPlayer.disp_d = {x:0, y:0}
       } else {
-        newPlayer.jumping = false;
-        newPlayer.pos.y = GND - 100
-        newPlayer.v.y = 0
+        newPlayer.disp_d = {x:0, y: Math.abs(Math.sin(newPlayer.t * 10)) * -10}
       }
 
-      newPlayer.disp_d = {x:0, y:0}
-    } else {
-      newPlayer.disp_d = {x:0, y: Math.abs(Math.sin(newPlayer.t * 10)) * -10}
     }
 
     const newEnemies = game.enemies.map((enemy: Enemy) => {
-      return {
-        pos: {
-          x: enemy.pos.x + dt * (-300),
-          y: enemy.pos.y
-        },
-      }
+      return stepEnemy(enemy, dt)
     }).filter((enemy: Enemy) => {
-      return enemy.pos.x >= -100
+      return enemy.pos.x >= -200 && enemy.pos.x <= 800
     })
 
     // spawn
     if (game.t > newSpawner.prevT + 2.0) {
-      newEnemies.push({ pos: { x: 700, y: 450 + 24 }})
+      newEnemies.push(nextEnemy())
       newSpawner.prevT += 2.0
+    }
+
+    const hit = game.enemies.some((enemy: Enemy) => {
+      return Math.pow(enemy.pos.x - player.pos.x, 2) + Math.pow(enemy.pos.y - player.pos.y, 2) < Math.pow(ENEMY_R + PLAYER_R, 2)
+    })
+    if (hit) {
+      newPlayer.fated = true
     }
 
     setGame({
@@ -168,15 +276,21 @@ export default function Home() {
       spawner: newSpawner,
       player: newPlayer,
       enemies: newEnemies,
+      gameOver: game.gameOver || hit,
     })
+
+    // かなり良くない書き方だけど。。
+    if ("Space" in currentKeyboardState && currentKeyboardState["Space"]) {
+      handleClick()
+    }
   });
 
   const jump = () => {
     console.log("jump")
     if (!game.player.jumping) {
       //const newPlayer = player
-      game.player.jumping = true;
-      game.player.v.y = -1000;
+      game.player.jumping = true
+      game.player.v.y = -1000
       setGame(game)
     }
   }
@@ -185,11 +299,8 @@ export default function Home() {
     jump()
   }
 
-  const keyPress = (e) => {
-    console.log(e)
-    if (e.which == 32) { // space
-      jump()
-    }
+  const handleRetry = () => {
+    console.log("handle retry")
   }
 
   const W = 600;
@@ -198,8 +309,19 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <GameStageView handleClick={() => handleClick()} width={W} height={H} game={game} />
+        <GameStageView handleClick={() => handleClick()} handleRetry={handleRetry} width={W} height={H} game={game} />
       </div>
+      <div>
+      <div>Pressed keys:</div>
+      <ul>
+        {Object.keys(currentKeyboardState).map((key) => (
+          <li key={key}>
+            {key}: {currentKeyboardState[key].toString()}
+          </li>
+        ))}
+      </ul>
+    </div>
+    <KeyboardEventHandler />
     </main>
   )
 }
