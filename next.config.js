0dea9cc8ed0bfe94e16ae3e9151b4dbf2742a8a9
oginom <oginomaa@gmail.com>
2023-05-13 21:19:21 +0900

    fix assetPrefix

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   distDir: 'build',
+  assetPrefix: '/dyno-jump',
   basePath: '/dyno-jump',
   output: 'export',
   images: {
     unoptimized: true,
-    path: `/dyno-jump/_next/image`,
   }
 }
 
